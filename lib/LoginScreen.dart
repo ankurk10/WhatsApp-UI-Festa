11b7cfec431275c916573a69c980922954ac1fff
ankurk10 <ankurkumarkushwaha.10@gmail.com>
2021-09-15 16:57:35 +0530

    Added Login Screen

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/HomeScreen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key key}) : super(key: key);
@@ -19,6 +20,70 @@ class LoginScreen extends StatelessWidget {
                 color: Colors.grey[800],
                 fontSize: 20,
                 fontWeight: FontWeight.w600,
+              ),
+            ),
+
+            SizedBox(
+              height: 10,
+            ),
+
+            Container(
+              margin: EdgeInsets.only(left: 20 , top: 20, right: 20),
+              child: Text("WhatsApp will send an SMS message to verify "
+                  "your phone number.",
+              style: TextStyle(
+                fontSize: 15,
+              ),
+              ),
+            ),
+
+            SizedBox(
+              height: 50,
+            ),
+
+
+            Container(
+              margin: EdgeInsets.only(left: 20 , top: 20, right: 20),
+              child: TextFormField(
+                cursorColor: Colors.black,
+                decoration: new InputDecoration(
+                    // border: InputBorder.none,
+                    // focusedBorder: InputBorder.none,
+                    // enabledBorder: InputBorder.none,
+                    // errorBorder: InputBorder.none,
+                    // disabledBorder: InputBorder.none,
+                    contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                    hintText: "Enter your phone number"),
+              ),
+            ),
+
+            SizedBox(
+              height: 150,
+            ),
+
+            ButtonTheme(
+              height: 40,
+              minWidth: 150,
+              child: RaisedButton(
+                child: Text("NEXT",
+                  style: TextStyle(
+                      color: Colors.black
+                  ),
+                ),
+                focusColor: Colors.teal,
+                highlightColor: Colors.yellow,
+                color: Colors.teal,
+                hoverColor: Colors.grey,
+                elevation: 20,
+                onPressed: ()
+                {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context){
+                        return HomeScreen();
+                      }
+                  ));
+                },
               ),
             ),
           ],
